b7ba3cd3854e055f00b90b7013d044063becd0bb
Audrius <adotbyte@gmail.com>
2026-08-03 11:07:34 +0300

    fix: fb_app_id on layout.tsx

--- frontend/src/app/[locale]/layout.tsx
+++ frontend/src/app/[locale]/layout.tsx
@@ -29,6 +29,7 @@ export default async function LocaleLayout({
   return (
     <html lang={locale} suppressHydrationWarning>
       <head>
+        <meta property="fb:app_id" content="914320204924075" />
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="icon" href="/favicon-16x16.png" sizes="16x16" type="image/png" />
         <link rel="icon" href="/favicon-32x32.png" sizes="32x32" type="image/png" />
